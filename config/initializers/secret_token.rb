11e483d973c1f713486c1f771191f7eaa308c5a1
Ben Gruber <blgruber@gmail.com>
2014-12-02 17:35:11 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rotten::Application.config.secret_key_base = '97c0d489529aa674c156cf9c3a65d604a706408620b157989c364c8571e8df41f473a0d5025eeeae72aa9f35204160800057d9e46035b35b88d1850e97a66353'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rotten::Application.config.session_store :cookie_store, key: '_rotten_session'
